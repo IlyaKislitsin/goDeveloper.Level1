726044a382a90d0457ae70f3e9002737cd4ba131
kinsman <ilya.kislitsin@gmail.com>
2021-04-01 23:53:05 +0500

    Update package. Calculating the area of a rectangle

--- lesson2/rectangleArea/main.go
+++ lesson2/rectangleArea/main.go
@@ -3,24 +3,26 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
 	fmt.Print("Найдём площадь прямоугольника.\n", "Введите длину первой стороны: ")
-	a, _ := scanNumber()
+	a, _ := getNumber()
 
 	fmt.Print("Введите длину второй стороны: ")
-	b, _ := scanNumber()
+	b, _ := getNumber()
 
 	fmt.Printf("Площадь прямоугольника равна: %d", a*b)
 }
 
-func scanNumber() (uint32, error) {
-	var number uint32
+func getNumber() (int64, error) {
+	var s string
 	for {
-		_, err := fmt.Scan(&number)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Возникла ошибка: %s\nПопробуйте ещё раз: ", err)
+		_, scanErr := fmt.Scan(&s)
+		number, err := strconv.ParseInt(s, 10, 64)
+		if scanErr != nil || err != nil || number <= 0 {
+			fmt.Fprintf(os.Stderr, "Ошибка: введены некорректные данные. Попробуйте ещё раз: : ")
 			continue
 		}
 		return number, nil
